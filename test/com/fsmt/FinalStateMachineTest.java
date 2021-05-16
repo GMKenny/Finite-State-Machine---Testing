5827a9caee86a13ac4bb5ccccbf01960474465bd
GMKenny <kenny.vandenberg@student.hu.nl>
2021-05-16 16:22:50 +0200

    Adding skeleton test unit files.
    2 files containing the different functions that will be unit tested

---
+++ test/com/fsmt/FinalStateMachineTest.java
@@ -0,0 +1,24 @@
+package com.fsmt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FinalStateMachineTest {
+
+    @Test
+    public void createNodes() {
+    }
+
+    @Test
+    public void setInputText() {
+    }
+
+    @Test
+    public void setConnectionPerNode() {
+    }
+
+    @Test
+    public void processInputToString() {
+    }
+}
---
+++ test/com/fsmt/NodeTest.java
@@ -0,0 +1,20 @@
+package com.fsmt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NodeTest {
+
+    @Test
+    public void getNodeName() {
+    }
+
+    @Test
+    public void createConnection() {
+    }
+
+    @Test
+    public void nextNode() {
+    }
+}
