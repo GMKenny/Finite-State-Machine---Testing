556a33a56721f3160506a9a35ab6ff93f6b3d000
GMKenny <kenny.vandenberg@student.hu.nl>
2021-05-16 20:25:40 +0200

    Unit test FSM functions
    1. createNodes test of de grote van de aantal nodes klopt aan de gegeven parameter.
    2. setinput controleerd de geven input voor fsm.
    3. setConnectionPerNode controleert of de geven connecties tussen node duplicaten kunnen zijn.
    4. processInputToSting controleert of de geven input een error op levert als de connectie na een aantal goede nodes niet meer klopt.

--- test/com/fsmt/FinalStateMachineTest.java
+++ test/com/fsmt/FinalStateMachineTest.java
@@ -8,17 +8,45 @@ public class FinalStateMachineTest {
 
     @Test
     public void createNodes() {
+        FinalStateMachine fSM = new FinalStateMachine(5);
+        System.out.println(fSM.getNodeSize());
     }
 
     @Test
     public void setInputText() {
+        FinalStateMachine fSM = new FinalStateMachine(3);
+        fSM.setInputText("ABA");
+        System.out.println(fSM.getInputText());
     }
 
     @Test
     public void setConnectionPerNode() {
+        FinalStateMachine fSM = new FinalStateMachine(3);
+        // Test input.
+        fSM.setInputText("A");
+        // duplicate test.
+        fSM.setConnectionPerNode("A", 1, 2);
+        fSM.setConnectionPerNode("A", 2, 3);
+        fSM.setConnectionPerNode("B", 2, 3);
+
+        System.out.println(fSM.processInputToString());
     }
 
     @Test
     public void processInputToString() {
+        FinalStateMachine fSM = new FinalStateMachine(3);
+        // Test input.
+        fSM.setInputText("CAABAAB");
+        // No connection test.
+        fSM.setConnectionPerNode("A", 1, 2);
+        fSM.setConnectionPerNode("B", 1, 3);
+        fSM.setConnectionPerNode("C", 1, 1);
+        fSM.setConnectionPerNode("A", 2, 1);
+        fSM.setConnectionPerNode("A", 3, 1);
+
+        System.out.println(fSM.processInputToString());
+
+
+
     }
 }
