541d1285daef05349a69cda5b70b4c825be4fd78
GMKenny <kenny.vandenberg@student.hu.nl>
2021-05-16 20:21:37 +0200

    Unit test node functions
    1. getNodeName controleerd of de naam in de variable word gevuld.
    2. createConnection contorleerd of er duplicaten in de connectie lijst gestopt kan worden.
    3. Functie next Node is te klein om te testen.

--- test/com/fsmt/NodeTest.java
+++ test/com/fsmt/NodeTest.java
@@ -8,13 +8,21 @@ public class NodeTest {
 
     @Test
     public void getNodeName() {
+        Node nodeOne = new Node("N1");
+        System.out.println(nodeOne.getNodeName());
     }
 
     @Test
     public void createConnection() {
+        Node nodeOne = new Node("N1");
+        Node nodeTwo = new Node("N2");
+        Node nodeThree = new Node("N3");
+        // duplicate test
+        nodeOne.createConnection("A",nodeTwo);
+        nodeOne.createConnection("A",nodeThree);
+        System.out.println(nodeTwo);
+        System.out.println(nodeThree);
+        System.out.println(nodeOne.nextNode("A"));
     }
-
-    @Test
-    public void nextNode() {
-    }
+    
 }
